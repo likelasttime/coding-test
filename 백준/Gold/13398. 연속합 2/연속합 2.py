21c3289ba01ef275e80ca55b6f24507a396dc03e
likelasttime <46569105+likelasttime@users.noreply.github.com>
2023-03-05 17:13:37 +0900

    [Gold V] Title: 연속합 2, Time: 136 ms, Memory: 38964 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.py
+++ 백준/Gold/13398. 연속합 2/연속합 2.py
@@ -1,17 +1,17 @@
+# 틀렸습니다
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 nums = list(map(int, input().split()))
 dp = [[0] * n for _ in range(2)]
-dp[0][0] = nums[0]
+dp[0][0], dp[1][0] = nums[0], nums[0]
 
 if n > 1 :
-    ans = -1e9
     for i in range(1, n) :
         dp[0][i] = max(dp[0][i - 1] + nums[i], nums[i])
         dp[1][i] = max(dp[0][i - 1], dp[1][i - 1] + nums[i])
-        ans = max(ans, dp[0][i], dp[1][i])
-    print(ans)
+
+    print(max(map(max, dp)))
 else :
     print(dp[0][0])
