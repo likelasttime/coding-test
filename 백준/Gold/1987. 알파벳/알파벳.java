00eb4dd6b2870668ce93d05376ddcca700c2e79b
likelasttime <46569105+likelasttime@users.noreply.github.com>
2023-08-18 13:28:53 +0900

    [Gold IV] Title: 알파벳, Time: 788 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static int R, C, ans;
+    static char[][] board;
+    static int[][] visit;
+    static final int[] dx={0, 0, 1, -1};
+    static final int[] dy={1, -1, 0, 0};
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        R=Integer.parseInt(st.nextToken());
+        C=Integer.parseInt(st.nextToken());
+        board=new char[R][C];
+        visit=new int[R][C];
+        for(int i=0; i<R; i++){
+            board[i]=br.readLine().toCharArray();
+        }
+
+        dfs(0, 0, 1 << board[0][0]-'A', 1);
+        System.out.println(ans);
+    }
+
+    public static void dfs(int x, int y, int bit, int cnt){
+        //if(visit[x][y] == bit) return;
+        //visit[x][y]=bit;
+        ans=Math.max(ans, cnt);
+        for(int i=0; i<4; i++){
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+            if(nx<0 || ny<0 || nx==R || ny==C || (bit&(1<<board[nx][ny]-'A')) != 0) continue;
+            dfs(nx, ny, bit | (1<<board[nx][ny]-'A'), cnt+1);
+        }
+    }
+}
