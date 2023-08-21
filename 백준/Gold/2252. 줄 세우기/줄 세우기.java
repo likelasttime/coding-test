4bbbcf18a53853bdc17a173d55af2949f1138f59
likelasttime <46569105+likelasttime@users.noreply.github.com>
2023-08-21 23:28:32 +0900

    [Gold III] Title: 줄 세우기, Time: 440 ms, Memory: 45652 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -10,42 +10,51 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+	static Queue<Integer> queue = new LinkedList<>();
+	static int n; // 학생 수
+	static ArrayList<ArrayList<Integer>> arrayList; // 인접 리스트(유향)
+	static int[] cntmap;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 
 		st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken()); // 학생 수
+		n = Integer.parseInt(st.nextToken()); // 학생 수
 		int m = Integer.parseInt(st.nextToken()); // 키를 비교한 횟수
 
-		ArrayList<ArrayList<Integer>> arrayList = new ArrayList<ArrayList<Integer>>(); // 인접 리스트(유향)
-		for (int i = 0; i <= n; i++) {
+		arrayList = new ArrayList<ArrayList<Integer>>();
+		for(int i=0; i<=n; i++) {
 			arrayList.add(new ArrayList<Integer>());
 		}
-
-		Integer[] cntmap = new Integer[n + 1];
+		
+		cntmap = new int[n + 1];
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			arrayList.get(a).add(b); // a가 b 앞에 있다.
-			if (cntmap[b] == null) {		// 앞에 선 사람이 없을 때
+			if(cntmap[b] == 0) {
 				cntmap[b] = 0;
 			}
-			cntmap[b]++; // 앞에 선 사람의 수 증가
+			cntmap[b]++; // 선수 과목 수 증가
 		}
 
 		/*
 		 * 진입 차수가 0인 노드를 큐에 모두 넣기
 		 */
-		Queue<Integer> queue = new LinkedList<>();
 		for (int i = 1; i <= n; i++) {
-			if (cntmap[i] == null) { 	// 앞에 선 사람이 없다.
+			if (cntmap[i] == 0) { // 선수 과목이 없다.
 				queue.offer(i);
 			}
 		}
 
+		bfs();
+		bw.flush();
+	}
+
+	public static void bfs() throws IOException {
 		while (!queue.isEmpty()) { 			// 큐가 안 비었으면
 			int num = queue.poll();
 			bw.write(num + " ");
@@ -56,6 +65,5 @@ public class Main {
 					}
 				}
 			}
-		bw.flush();			// 정답 출력
 	}
 }
