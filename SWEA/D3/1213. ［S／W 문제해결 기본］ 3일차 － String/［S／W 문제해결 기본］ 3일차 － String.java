fe1bb9869581160c26167018238aaaaf0ba59106
likelasttime <46569105+likelasttime@users.noreply.github.com>
2024-10-20 21:33:23 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 154 ms, Memory: 22,172 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -1,37 +1,52 @@
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
-class Solution
-{
-    public static void main(String args[]) throws Exception
-    {
+class Solution {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        int T = 10;
-        //T=sc.nextInt();
+        int T = 10; // 테스트 케이스 수
 
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            int test_num = sc.nextInt();		// 테스트 케이스 번호
-            String keyword = sc.next();		// 찾을 문자열
-            String sentence = sc.next();	// 검색할 문장
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int test_num = sc.nextInt();  // 테스트 케이스 번호
+            String keyword = sc.next();    // 찾을 문자열
+            String sentence = sc.next();    // 검색할 문장
 
-            int answer = 0;		// 빈도 수
-            int len = keyword.length();		// 찾을 문자열 길이
-            int left = 0;		// 검색할 문장 인덱스
-            int cnt = 0;		// 찾을 문자열 인덱스
-            while(left <= sentence.length() - len) {		
-                cnt = 0;
-                while(cnt < len && sentence.charAt(left + cnt) == keyword.charAt(cnt)) {		// 문자가 같으면
-                    cnt++;
-                }
+            int answer = 0;                // 빈도 수
+            int len = keyword.length();     // 찾을 문자열 길이
+            Map<Character, Integer> skip = new HashMap<>();
 
-                if(cnt == len) {		// 문자열을 찾았다면
-                    left += cnt;		// 시작점을 길이만큼 건너뛰기
-                    answer++;
-                } else {		// 문자열을 못 찾았다면
-                    left++;
+            // skip 만들기
+            for (int i = 0; i < len; i++) {
+                skip.put(keyword.charAt(i), len - i - 1);
+            }
+
+            int start = len - 1;  // 검색할 문장 인덱스
+            while (start <= sentence.length() - 1) {
+                if (sentence.charAt(start) != keyword.charAt(len - 1)) { // 마지막 문자가 일치하지 않는다면
+                    // 이 문자가 skip에 있으면
+                    if (skip.containsKey(sentence.charAt(start))) {
+                        start += skip.get(sentence.charAt(start));
+                    } else { // skip에 없으면
+                        start += len; // 찾으려는 문자의 길이만큼 건너뛰기
+                    }
+                } else { // 문자가 일치하면 그 다음 문자 비교
+                    boolean flag = false;
+                    for (int i = len - 2; i >= 0; i--) {
+                        if (sentence.charAt(start - (len - 1 - i)) != keyword.charAt(i)) {
+                            flag = true;
+                            break;
+                        }
+                    }
+
+                    if (!flag) {
+                        answer++;
+                    }
+
+                    start += len; // 다음 위치로 이동
                 }
             }
-            
+
             System.out.println("#" + test_case + " " + answer);
         }
     }
