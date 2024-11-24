0249c927e8fed087a0290fc79b0055d71983faac
likelasttime <46569105+likelasttime@users.noreply.github.com>
2024-11-24 20:35:04 +0900

    [level 3] Title: 리틀 프렌즈 사천성, Time: 52.86 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/3/1836. 리틀 프렌즈 사천성/리틀 프렌즈 사천성.java
+++ 프로그래머스/3/1836. 리틀 프렌즈 사천성/리틀 프렌즈 사천성.java
@@ -1,133 +1,214 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.TreeSet;
 import java.util.Set;
 import java.util.Arrays;
 
 class Solution {
-    static char[][] arr;
-    static int[][] posX;
-    static int[][] posY;
+    static int rowSize;
+    static int colSize;
+    static int[][] pos1;
+    static int[][] pos2;
+    static Set<Character> treeSet;
+    final static int[] dx = {-1, 1, 0, 0};
+    final static int[] dy = {0, 0, -1, 1};
+
+    public boolean isValid(int nx, int ny) {
+        return 0 <= nx && nx < rowSize && 0 <= ny && ny < colSize;
+    }
+
+    /*
+        좌 또는 우로 한번 꺾기
+    */
+    public boolean removeTurnLineLeftOrRight(int x, int y, char[][] boardArr) {
+        for(int d=2; d<4; d++) {
+            int nx = dx[d] + x;
+            int ny = dy[d] + y;
+
+            if(!isValid(nx, ny) || boardArr[nx][ny] == '*') {
+                continue;
+            }
+
+            while(isValid(nx, ny) && boardArr[nx][ny] != '*' && boardArr[nx][ny] == '.') {
+                // 위로 꺾기
+                int topNx = nx;
+                int topNy = ny;
+                while(topNx >= 0) {
+                    if(boardArr[topNx][topNy] == boardArr[x][y]) {
+                        boardArr[topNx][topNy] = '.';
+                        boardArr[x][y] = '.';
+                        return true;
+                    } else if(boardArr[topNx][topNy] != '.') {
+                        break;
+                    }
+                    topNx--;
+                }
+
+                // 아래로 꺾기
+                int bottomNx = nx;
+                int bottomNy = ny;
+                while(bottomNx < rowSize) {
+                    if(boardArr[bottomNx][bottomNy] == boardArr[x][y]) {
+                        boardArr[bottomNx][bottomNy] = '.';
+                        boardArr[x][y] = '.';
+                        return true;
+                    } else if(boardArr[bottomNx][bottomNy] != '.') {
+                        break;
+                    }
+                    bottomNx++;
+                }
+
+                nx += dx[d];
+                ny += dy[d];
+            }
+        }
+        return false;
+    }
+
+    /*
+        위 또는 아래로 한번 꺾기
+    */
+    public boolean removeTurnLineUpOrDown(int x, int y, char[][] boardArr) {
+        for(int d=0; d<2; d++) {
+            int nx = dx[d] + x;
+            int ny = dy[d] + y;
+
+            if(!isValid(nx, ny) || boardArr[nx][ny] == '*') {
+                continue;
+            }
+
+            while(isValid(nx, ny) && boardArr[nx][ny] != '*' && boardArr[nx][ny] == '.') {
+                // 좌로 꺾기
+                int topNx = nx;
+                int topNy = ny;
+                while(topNy >= 0) {
+                    if(boardArr[topNx][topNy] == boardArr[x][y]) {
+                        boardArr[topNx][topNy] = '.';
+                        boardArr[x][y] = '.';
+                        return true;
+                    } else if(boardArr[topNx][topNy] != '.') {
+                        break;
+                    }
+                    topNy--;
+                }
+
+                // 우로 꺾기
+                int bottomNx = nx;
+                int bottomNy = ny;
+                while(bottomNy < colSize) {
+                    if(boardArr[bottomNx][bottomNy] == boardArr[x][y]) {
+                        boardArr[bottomNx][bottomNy] = '.';
+                        boardArr[x][y] = '.';
+                        return true;
+                    } else if(boardArr[bottomNx][bottomNy] != '.') {
+                        break;
+                    }
+                    bottomNy++;
+                }
+
+                nx += dx[d];
+                ny += dy[d];
+            }
+        }
+        return false;
+    }
+
+    /*
+        수직 또는 수평으로 없애기
+    */
+    public boolean removeLine(int x, int y, char[][] boardArr) {
+        for(int d=0; d<4; d++) {
+            int nx = dx[d] + x;
+            int ny = dy[d] + y;
+
+            while(isValid(nx, ny)) {
+                if(boardArr[nx][ny] == boardArr[x][y]) {      // 제거할 타일을 찾음
+                    boardArr[x][y] = '.';
+                    boardArr[nx][ny] = '.';
+                    return true;
+                }
+
+                // 못 가는 칸이라면
+                if(boardArr[nx][ny] != '.') {
+                    break;
+                }
+
+                nx += dx[d];
+                ny += dy[d];
+            }
+        }
+        return false;
+    }
 
     public String solution(int m, int n, String[] board) {
-        Set<Character> treeSet = new TreeSet();
-        StringBuilder sb = new StringBuilder();
-        arr = new char[m][n];
-        posX = new int[26][2];     // 각 문자별로 x 좌표 저장
-        posY = new int[26][2];     // 각 문자별로 y 좌표 저장
-        
-        // String -> char[]
-        for(int i=0; i<m; i++) {        // 행
-            arr[i] = board[i].toCharArray();
-        }
-        
-        // 초기화
+        StringBuilder answer = new StringBuilder();
+        rowSize = m;
+        colSize = n;
+        char[][] boardArr = new char[m][n];
+        pos1 = new int[26][2];
+        pos2 = new int[26][2];
+        treeSet = new TreeSet();
+
+        // 배열 초기화
         for(int i=0; i<26; i++) {
-            Arrays.fill(posX[i], -1);
-            Arrays.fill(posY[i], -1);
+            Arrays.fill(pos1[i], -1);
         }
-        
-        // 문자별 위치 할당
-        for(int x=0; x<m; x++) {        // 행
-            for(int y=0; y<n; y++) {    // 열
-                if(Character.isUpperCase(arr[x][y])) {      // 대문자가 맞다면
-                    treeSet.add(arr[x][y]);     // 대문자 저장
-                    int c = arr[x][y] - 'A';
-                    if(posX[c][0] == -1) {     // 첫 번째로 등장한 문자라면
-                        posX[c][0] = x;        // 첫 번째로 발견한 문자의 행 저장
-                    } else {        // 같은 문자를 발견했을 때
-                        posX[c][1] = x;        // 첫 번째로 발견한 문자의 행 저장
+
+        for(int x=0; x<m; x++) {        // 세로
+            boardArr[x] = board[x].toCharArray();
+            for(int y=0; y<n; y++) {        // 가로
+                if(boardArr[x][y] != '.' && boardArr[x][y] != '*') {
+                    int idx = boardArr[x][y] - 'A';
+                    if (pos1[idx][0] == -1) {
+                        pos1[idx][0] = x;
+                        pos1[idx][1] = y;
+                    } else {
+                        pos2[idx][0] = x;
+                        pos2[idx][1] = y;
                     }
-                    
-                    if(posY[c][0] == -1) {     // 첫 번째로 등장한 문자라면
-                        posY[c][0] = y;     // 첫 번째로 발견한 문자의 열 저장
-                    } else {        // 같은 문자를 발견했을 때
-                        posY[c][1] = y;        // 열 저장
-                    }
+                    treeSet.add(boardArr[x][y]);
                 }
             }
         }
-        
-        while(!treeSet.isEmpty()) {     // 대문자를 모두 제거할 때까지
-            boolean flag = false;
-            int size = treeSet.size();
 
-            for(char c : treeSet) {     // 오름차순 정렬된 순서대로 꺼내기
-                if(check(c)) {
-                    flag = true;
-                    sb.append(c);       // 제거 후 문자 추가
-                    treeSet.remove(c);  // 다음 번에 탐색하지 않아도 되니까 제거
+        while(true) {
+            boolean isEnd = true;
+            for(char ch : treeSet) {
+                int x = pos1[ch - 'A'][0];
+                int y = pos1[ch - 'A'][1];
+                if (boardArr[x][y] == '.' || boardArr[x][y] == '*') {
+                    continue;
+                }
+                if (removeLine(x, y, boardArr)) {
+                    isEnd = false;
+                    answer.append(ch);
+                    treeSet.remove(ch);
+                    break;
+                } else if (removeTurnLineUpOrDown(x, y, boardArr)) {
+                    isEnd = false;
+                    answer.append(ch);
+                    treeSet.remove(ch);
+                    break;
+                } else if (removeTurnLineLeftOrRight(x, y, boardArr)) {
+                    isEnd = false;
+                    answer.append(ch);
+                    treeSet.remove(ch);
                     break;
                 }
             }
 
-            if(!flag) {     // 제거 작업이 하나도 일어나지 않았다면
+            // 다 돌았는데 제거한 게 하나도 없다면
+            if(isEnd) {
                 break;
             }
         }
-        return treeSet.size() > 0 ? "IMPOSSIBLE" : sb.toString();
-    }
-    
-    public boolean check(char curChar) {
-        boolean result = false;
-        if(Character.isUpperCase(curChar)) {      // 대문자라면
-            int curIdx = curChar - 'A';
-            int x1 = posX[curIdx][0];
-            int x2 = posX[curIdx][1];
-            int y1 = posY[curIdx][0];
-            int y2 = posY[curIdx][1];
-                    
-            // 조건을 충족하면 제거
-            if(x1 != x2 && y1 != y2) {      // 꺾어야 할때
-                if(checkX(x1, x2, y2, curChar) && checkY(y1, y2, x1, curChar)) {
-                    arr[x1][y1] = '.';
-                    arr[x2][y2] = '.';
-                    result = true;
-                }
-                        
-                if(checkX(x1, x2, y1, curChar) && checkY(y1, y2, x2, curChar)) {
-                    arr[x1][y1] = '.';
-                    arr[x2][y2] = '.';
-                    result = true;
-                }
-            } else if(x1 == x2 && y1 != y2) {       // x좌표가 동일할 때
-                if(checkY(y1, y2, x1, curChar)) {
-                    arr[x1][y1] = '.';
-                    arr[x2][y2] = '.';
-                    result = true;
-                }
-            } else if(x1 != x2 && y1 == y2) {        // y좌표가 동일할 때
-                if(checkX(x1, x2, y1, curChar)) {
-                    arr[x1][y1] = '.';
-                    arr[x2][y2] = '.';
-                    result = true;
-                }
-            } 
+
+        // 다 제거했는지
+        if(!treeSet.isEmpty()) {
+            return "IMPOSSIBLE";
         }
-        return result;
-    }
-    
-    public boolean checkX(int x1, int x2, int y, char c) {
-        int start = Math.min(x1, x2);
-        int end = Math.max(x1, x2);
-        
-        for(int i=start; i<=end; i++) {
-            if(arr[i][y] != '.' && arr[i][y] != c) {        // 빈칸이 아니고, 같은 문자도 아니라면
-                return false;        // 같은 문자가 아님
-            }
-        }
-        
-        return true;   // 같은 문자를 찾음
-    }
-    
-    public boolean checkY(int y1, int y2, int x, char c) {
-        int start = Math.min(y1, y2);
-        int end = Math.max(y1, y2);
-        
-        for(int i=start; i<=end; i++) {
-            if(arr[x][i] != '.' && arr[x][i] != c) {    // 빈칸이 아니고, 같은 문자도 아니라면
-                return false;        // 같은 문자가 아님
-            }
-        }
-        
-        return true;
+
+        return answer.toString();
     }
 }
