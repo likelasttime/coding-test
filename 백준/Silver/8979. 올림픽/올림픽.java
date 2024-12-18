88d01334fa9bcbb1096cadddeb2b39d52109bdd1
likelasttime <46569105+likelasttime@users.noreply.github.com>
2024-12-18 20:45:51 +0900

    [Silver V] Title: 올림픽, Time: 184 ms, Memory: 19816 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Collections;
 import java.util.List;
@@ -22,51 +21,56 @@ class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();       // 1 <= 국가의 수 <= 1,000
-        int k = sc.nextInt();       // 1 <= 알고 싶은 국가 <= n
-        int answer = 0;
-        List<Country> countries = new ArrayList();
+        int n = sc.nextInt();  // 국가의 수
+        int k = sc.nextInt();  // 알고 싶은 국가 번호
+        List<Country> countries = new ArrayList<>();
 
-        for(int i=1; i<=n; i++) {
-            int country = sc.nextInt();     // 국가 번호
+        // 국가 데이터 입력
+        for (int i = 1; i <= n; i++) {
+            int country = sc.nextInt();  // 국가 번호
             countries.add(new Country(country, sc.nextInt(), sc.nextInt(), sc.nextInt()));
         }
 
+        // 메달 수 기준으로 국가들을 정렬
         Collections.sort(countries, new Comparator<Country>() {
             @Override
             public int compare(Country c1, Country c2) {
-                if(c1.gold != c2.gold) {
+                if (c1.gold != c2.gold) {
                     return Integer.compare(c2.gold, c1.gold);
                 }
-                if(c1.silver != c2.silver) {
+                if (c1.silver != c2.silver) {
                     return Integer.compare(c2.silver, c1.silver);
                 }
                 return Integer.compare(c2.iron, c1.iron);
             }
         });
 
-        int idx = 0;
+        int rank = 1;
         int prevGold = -1;
         int prevSilver = -1;
         int prevIron = -1;
-        int cnt = 0;    // 연속 횟수
-        while(true) {
-            Country country = countries.get(idx);
-            if(country.idx == k) {
+
+        // 순위 매기기
+        for (int i = 0; i < n; i++) {
+            Country country = countries.get(i);
+
+            // 랭킹 계산: 동일한 메달 수치는 랭킹이 동일하게 유지됨
+            if (country.gold == prevGold && country.silver == prevSilver && country.iron == prevIron) {
+                // 랭킹을 증가시키지 않음
+            } else {
+                rank = i + 1;  // 새로운 랭킹은 i+1
+            }
+
+            // 국가가 k라면, 해당 국가의 순위를 출력하고 종료
+            if (country.idx == k) {
+                System.out.println(rank);
                 break;
             }
-            if(country.gold == prevGold && country.silver == prevSilver
-                    && country.iron == prevIron) {
-                cnt++;
-            } else {
-                prevGold = country.gold;
-                prevSilver = country.silver;
-                prevIron = country.iron;
-            }
-            answer++;
-            idx++;
-        }
 
-        System.out.println(answer - cnt);
+            // 이전 메달 정보를 업데이트
+            prevGold = country.gold;
+            prevSilver = country.silver;
+            prevIron = country.iron;
+        }
     }
 }
