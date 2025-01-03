6406de908a38891e75d2635b7c4855fefbd5143f
likelasttime <46569105+likelasttime@users.noreply.github.com>
2025-01-03 23:18:40 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 213 ms, Memory: 43,468 KB -BaekjoonHub

--- SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -1,113 +1,92 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Deque;
+import java.io.*;
+import java.util.Queue;
 import java.util.LinkedList;
 
-class Solution {
-    /** 왼쪽부터 순차적으로 사방탐색 */
-	static int[] dx = { -1, -1, -1, 0, 0, 1, 1, 1 };
-	static int[] dy = { -1, 0, 1, -1, 1, -1, 0, 1 };
-	static int n;		// 배열의 크기
-	static int[][] check;		// 1:지뢰, 2:아직 클릭하지 않은 . , 3:클릭한 . , 0: 8방으로 지뢰가 없는 .
-	static int answer;		// 최소 클릭 횟수
-	
-	public static void main(String args[]) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T;
-		T = Integer.parseInt(br.readLine()); // 테스트 케이스 수
+public class Solution {
+    final static int[] DX = {-1, 1, 0, 0, -1, 1, 1, -1};
+    final static int[] DY = {0, 0, -1, 1, 1, 1, -1, -1};
 
-		for (int test_case = 1; test_case <= T; test_case++) {
-			answer = 0;				// 최소 클릭 횟수
-			n = Integer.parseInt(br.readLine());		// 배열의 크기
-			check = new int[n][n]; 						// 지뢰, 클릭 상태를 체크하는 배열
-			String[][] arr = new String[n][n];
-			
-            /** 입력으로 주어지는 지뢰 정보 배열을 입력 받기 */
-            for (int i = 0; i < n; i++) {
-				String str = br.readLine();
-				for (int j = 0; j < n; j++) {
-					arr[i][j] = String.valueOf(str.charAt(j));		// 문자열에서 문자를 추출 후 문자열로 변환해서 저장
-					if (arr[i][j].equals("*")) { 						// 지뢰를 발견하면 1로 표시
-						check[i][j] = 1;
-					}
-				}
-			}
+    static int n;
+    static char[][] arr;
+    static int clickCnt;
+    static boolean[][] visit;
 
-			/**
-			 * 지뢰의 주변을 탐색
-             * 지뢰가 있으면 1
-             * 주위에 지뢰가 있는 .은 2
-             * 지뢰가 주변에 아예 없으면 0
-			 */
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					if (check[i][j] == 1) { 			// 지뢰를 발견
-						for (int d = 0; d < 8; d++) {		// 8방 탐색
-							int nx = i + dx[d];
-							int ny = j + dy[d];
-							if (0 > nx || n <= nx || 0 > ny || n <= ny) { 		// 범위 체크
-								continue;
-							}
-							if(arr[nx][ny].equals(".")) {			
-								check[nx][ny] = 2; 				// 주위에 지뢰가 있는 .
-							}
-						}
-					}
-				}
-			}
+    public static boolean isValid(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
 
-			/**
-            * 주위에 지뢰가 없는 곳(0)을 탐색
-            * 지뢰가 아닌 칸은 방문 후 3으로 표시됨
-            */
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < n; j++) {
-					if (check[i][j] == 0) { 		// 8방으로 지뢰가 없는 곳
-						bfs(i, j);
-						answer++;				// 클릭횟수 증가
-						}
-					}
-				}
-			
-            /** 아직 클릭하지 않은 곳은 값이 2이고, 한 번씩 누른다. */
-			for(int i=0; i<n; i++) {
-				for(int j=0; j<n; j++) {
-					if(check[i][j] == 2) {		// 아직 클릭하지 않은 곳
-						answer++;				// 클릭 횟수 증가
-					}
-				}
-			}
-			sb.append("#").append(test_case).append(" ").append(answer).append("\n");			// 최소 클릭 횟수 출력
-		}
-		System.out.print(sb);		// 모든 테스트 케이스 실행 후 한번에 출력
-	}
-	
-    /**
-    * 값이 0인 곳을 시작으로 해서 탐색 중 0인 곳을 또 만나면 큐에 넣고 계속 탐색
-    * 방문한 곳은 3으로 값을 바꾼다.
-    */
-	public static void bfs(int start, int end) {
-		Deque<int[]> que = new LinkedList();
-		que.add(new int[] {start, end});
-		check[start][end] = 3;
-		
-		while(!que.isEmpty()) {
-			int[] pos = que.poll();
-            for(int i=0; i<8; i++) {		// 8방 탐색
-				int nx = pos[0] + dx[i];
-				int ny = pos[1] + dy[i];			
-				
-                if(0 > nx || nx >= n || 0 > ny || ny >= n) {		// 범위 체크
-					continue;
-				}
-				
-				if(check[nx][ny] == 0) {		// 0이라면
-					que.add(new int[] {nx, ny});
-				}	
-                
-				check[nx][ny] = 3;			// 방문한 곳은 3으로 표시
-			}
-		}
-	}
+    public static boolean isNearBomb(int x, int y) {
+        int cnt = 0;
+        for(int d=0; d<8; d++) {
+            int nx = x + DX[d];
+            int ny = y + DY[d];
+            if(!isValid(nx, ny)) {
+                continue;
+            }
+            if(arr[nx][ny] == '*') {        // 지뢰
+                cnt++;
+            }
+        }
+        if(cnt == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public static void bfs(int x, int y) {
+        Queue<int[]> que = new LinkedList();
+        que.offer(new int[]{x, y});
+        visit[x][y] = true;
+        while(!que.isEmpty()) {
+            int[] cur = que.poll();
+            if(arr[cur[0]][cur[1]] == '*' || isNearBomb(cur[0], cur[1])) {
+                continue;
+            }
+            for(int d=0; d<8; d++) {
+                int nx = cur[0] + DX[d];
+                int ny = cur[1] + DY[d];
+                if(!isValid(nx, ny) || visit[nx][ny] || arr[nx][ny] == '*') {
+                    continue;
+                }
+                visit[nx][ny] = true;
+                que.offer(new int[]{nx, ny});
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());        // 전체 테스트 케이스의 수
+
+        for(int tc = 1; tc <= t; tc++) {
+            n = Integer.parseInt(br.readLine());
+            arr = new char[n][n];
+            for(int i=0; i<n; i++) {
+                arr[i] = br.readLine().toCharArray();
+            }
+            visit = new boolean[n][n];
+            clickCnt = 0;
+            // 주위에 지뢰가 없는 곳부터 클릭하기
+            for(int x=0; x<n; x++) {
+                for(int y=0; y<n; y++) {
+                    if(arr[x][y] == '*' || visit[x][y]) {  // 지뢰라면
+                        continue;
+                    }
+                    if(!isNearBomb(x, y)) {
+                        bfs(x, y);
+                        clickCnt++;
+                    }
+                }
+            }
+            // 남은 곳 클릭하기
+            for(int x=0; x<n; x++) {
+                for(int y=0; y<n; y++) {
+                    if(arr[x][y] != '*' && !visit[x][y]) {
+                        clickCnt++;
+                    }
+                }
+            }
+            System.out.println("#" + tc + " " + clickCnt);
+        }
+    }
 }
