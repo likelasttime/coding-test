fa157096a6abb85656b3a8510b9de9f665577ffb
likelasttime <46569105+likelasttime@users.noreply.github.com>
2025-01-25 16:56:08 +0900

    [Unrated] Title: [모의 SW 역량테스트] 특이한 자석, Time: 128 ms, Memory: 32,532 KB -BaekjoonHub

--- SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
+++ SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
@@ -1,144 +1,123 @@
 import java.util.Scanner;
 
-public class Solution {
-	
-	static final int numOfmagnet = 4;
-	static final int numOfteeth = 8;
-	static int magnet[][] = new int[5][8];		// 자석의 정보를 초기화 (자석이 4개지만 문제에서 자석의 번호는 1로 시작해서 편의상 5개로 만들었다)
-	static int turn[][] = new int[21][2];		// 자석의 회전 정보를 저장함. 최대 20번 돌아갈 수 있다.
-	static int magnetScorePointer[] = new int[5];		// 각 자석마다 빨간 화살표가 가리키는 날을 저장함
-	static int turningMagnets[] = new int[numOfmagnet + 1];		// 각 회전 수행 시 같이 돌아가는 자석의 방향을 저장
+class Solution {
+    final static int MAGNETIC_CNT = 4;
+    final static int NAL_CNT = 8;
 
-	/*
-	 * 회전될 자석 회전 작업(실제로는 빨간 화살표 위치를 바꿈)
-	 */
-	static void turnMagnet() {
-		for (int magnetNum = 1; magnetNum <= numOfmagnet; magnetNum++) {
-			int turningDirection = turningMagnets[magnetNum];
-			if (turningDirection == 1) {		// 시계방향 회전 <-
-				magnetScorePointer[magnetNum] = magnetScorePointer[magnetNum] == 0 ? numOfteeth - 1 : magnetScorePointer[magnetNum] - 1;
-			} else if (turningDirection == -1) {	// 반시계방향 회전 ->
-				magnetScorePointer[magnetNum] = magnetScorePointer[magnetNum] == numOfteeth - 1 ? 0 : magnetScorePointer[magnetNum] + 1;
-			}
-		}
-	}
-	
-	/*
-	 * 회전할 자석들 확인
-	 */
-	static void checkSide(int pivotMagnetNum, int rotateDirection, int side) {
-		int pivotMagneticPole, sideMagneticPole, pivotScorePointer, sideScorePointer, sideMagnetNum;
-		sideMagneticPole = 0;
-		pivotMagneticPole = 0;
-		
-		// 옆 자석을 연속해서 확인하기 위한 while loop
-		while (true) {
-			if (side == 1 && pivotMagnetNum == numOfmagnet) {	// 오른쪽 자석을 확인하는데 없으면 중지
-				break;
-			} else if (side == -1 && pivotMagnetNum == 1) {		// 왼쪽 자석을 확인하는데 없으면 중지	
-				break;
-			}
-			sideMagnetNum = pivotMagnetNum + side;		// 옆 자석
-			pivotScorePointer = magnetScorePointer[pivotMagnetNum];		// 중심 자석 빨간 화살표 위치
-			sideScorePointer = magnetScorePointer[sideMagnetNum];		// 사이드 자석 빨간 화살표 위치
-			
-			if (side == -1) {		// 왼쪽 자석 처리
-				pivotMagneticPole = pivotScorePointer < 2 ? magnet[pivotMagnetNum][numOfteeth + pivotScorePointer - 2] : magnet[pivotMagnetNum][pivotScorePointer - 2];
-				sideMagneticPole = sideScorePointer >= numOfteeth - 2 ? magnet[sideMagnetNum][sideScorePointer - numOfteeth + 2] : magnet[sideMagnetNum][sideScorePointer + 2];
-			} else if (side == 1) {		// 오른쪽 자석 처리
-				pivotMagneticPole = pivotScorePointer >= numOfteeth - 2 ? magnet[pivotMagnetNum][pivotScorePointer - numOfteeth + 2] : magnet[pivotMagnetNum][pivotScorePointer + 2];
-				sideMagneticPole = sideScorePointer < 2 ? magnet[sideMagnetNum][numOfteeth + sideScorePointer - 2] : magnet[sideMagnetNum][sideScorePointer - 2];
-			}
-			
-			if (sideMagneticPole != pivotMagneticPole) {		// 자성이 다르면 회전
-				turningMagnets[sideMagnetNum] = -rotateDirection;
-				rotateDirection = -rotateDirection;
-			} else {	// 자성이 다르면 더 이상 확인하지 않음
-				break;
-			}
-			pivotMagnetNum += side;		// 다음 옆 자석 확인
-		}
-	}
-	
-	/*
-	 * 자석의 점수를 구하기 위한 함수
-	 */
-	static int pow (int x, int n) {
-		int result = 1;
-		for (int i = 1; i < n; i++) {
-			result *= x;
-		}
-		return result;
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T, k, score, pivotMagnetNum, rotateDirection;
-		T = sc.nextInt();
-		// 테스트 케이스 입력 받음
-		
-		for (int tc = 1; tc<=T; tc++) {
-			// 점수를 0으로 초기화함
-			score = 0;
-			// 회전의 수를 저장함
-			k = sc.nextInt();
-			
-			// 각 자석의 정보를 초기화함
-			for (int i=1; i<=numOfmagnet; i++) {
-				for (int j=0; j<numOfteeth; j++) {
-					magnet[i][j] = sc.nextInt();
-				}
-			}
-			
-			// 회전할 자석과 방향의 정보를 초기화함
-			for (int i=1; i<=k; i++) {
-				turn[i][0] = sc.nextInt(); 			// magnet number
-				turn[i][1] = sc.nextInt(); 			// rotate direction
-			}
-			
-			// 빨간 화살표를 초기화함
-			for (int i=0; i<numOfmagnet + 1; i++) {
-				magnetScorePointer[i] = 0;
-			}
-			
-			// 자석 회전 실행
-			for (int i=1; i<=k; i++) {
-				// 같이 돌아가게 될 자석 초기화
-				for (int j=0; j<numOfmagnet + 1; j++) {
-					turningMagnets[j] = 0;
-				}
-				
-				pivotMagnetNum = turn[i][0];		// input으로 돌릴 자석
-				rotateDirection = turn[i][1];		// input으로 돌릴 자석의 방향
-				
-				// input으로 돌릴 자석 방향 저장
-				turningMagnets[pivotMagnetNum] = rotateDirection;
-				
-				// input으로 돌릴 자석의 오른쪽에 있는 자석 중 같이 돌아갈 자석 저장
-				if (pivotMagnetNum != numOfmagnet) {
-					checkSide(pivotMagnetNum, rotateDirection, 1);
-				}
-				
-				// input으로 돌릴 자석의 왼쪽에 있는 자석 중 같이 돌아갈 자석 저장
-				if (pivotMagnetNum != -1) {
-					checkSide(pivotMagnetNum, rotateDirection, -1);
-				}
-				
-				// 회전할 자석 회전
-				turnMagnet();
-			}
-			
-			// 점수 계산
-			for (int i=1; i<=numOfmagnet; i++) {
-				if (magnet[i][magnetScorePointer[i]] == 1) {
-					score += pow(2, i);
-				}
-			}
-			
-			// score 출력
-			System.out.println("#" + tc + " " + score);
-		}
-		sc.close();
-	}
+    static int[][] magnetic;
+    static int[][] pos;     // 각 자석의 빨간색 화살표 위치 저장
+    static int answer;      // 총 점수
+    static int[] rotateDirection;       // 각 자석별 회전 방향 저장
 
+    /*
+        반시계 방향 회전
+        num: 자석 번호
+     */
+    public static void rotateLeft(int num) {
+        pos[num][0] = (pos[num][0] + 1) % NAL_CNT;
+    }
+
+    /*
+        시계 방향 회전
+        num: 자석 번호
+     */
+    public static void rotateRight(int num) {
+        if(pos[num][0] == 0) {
+            pos[num][0] = NAL_CNT - 1;
+        } else {
+            pos[num][0]--;
+        }
+    }
+
+    /*
+        현재 자석 num에서 왼쪽으로 탐색하면서 같이 회전할 자석을 찾기
+     */
+    public static int getLeftRange(int num, int d) {
+        int leftRange = num;
+        int curD = d;
+        for(int i=num; i>=1; i--) {      // 자석 번호
+            int left = magnetic[i - 1][(pos[i - 1][0] + 2) % NAL_CNT];
+            int right = magnetic[i][(pos[i][0] + 6) % NAL_CNT];
+            if(left == right) {     // 자성이 같으면
+                return i;
+            }
+            curD = getDirection(curD);
+            rotateDirection[i - 1] = curD;
+            leftRange = i - 1;
+        }
+        return leftRange;
+    }
+
+    /*
+        현재 자석 num에서 오른쪽으로 탐색하면서 같이 회전할 자석을 찾기
+     */
+    public static int getRightRange(int num, int d) {
+        int rightRange = num;
+        int curD = d;
+        for(int i=num+1; i<MAGNETIC_CNT; i++) {      // 자석 번호
+            int right = magnetic[i][(pos[i][0] + 6) % NAL_CNT];
+            int left = magnetic[i - 1][(pos[i - 1][0] + 2) % NAL_CNT];
+            if(left == right) {     // 자성이 같으면
+                return i - 1;
+            }
+            curD = getDirection(curD);
+            rotateDirection[i] = curD;
+            rightRange = i;
+        }
+        return rightRange;
+    }
+
+    public static int getDirection(int d) {
+        if(d == 1) {
+            return -1;
+        }
+        return 1;
+    }
+
+    public static void rotate(int num, int d) {
+        int start = getLeftRange(num, d);
+        int end = getRightRange(num, d);
+        rotateDirection[num] = d;
+        for(int i=start; i<=end; i++) {
+            if(rotateDirection[i] == 1) {       // 시계 방향
+                rotateRight(i);
+            } else {        // 반시계 방향
+                rotateLeft(i);
+            }
+        }
+    }
+
+    public static void getAnswer() {
+        for(int i=0; i<MAGNETIC_CNT; i++) {
+            if(magnetic[i][pos[i][0]] == 1) {       // S극일때
+                answer += Math.pow(2, i);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();       // 테스트 케이스 수
+        for(int tc=1; tc<=t; tc++) {
+            magnetic = new int[MAGNETIC_CNT][NAL_CNT];
+            pos = new int[MAGNETIC_CNT][1];
+            rotateDirection = new int[MAGNETIC_CNT];
+            answer = 0;
+            int k = sc.nextInt();       // 자석을 회전시키는 횟수
+            // 1번 자석부터 4번 자석까지 각각 8개 날의 자성정보 입력받기
+            for(int i=0; i<4; i++) {
+                for(int j=0; j<8; j++) {
+                    magnetic[i][j] = sc.nextInt();
+                }
+            }
+            // 자석을 1칸씩 회전시키는 회전 정보 입력받기
+            for(int i=0; i<k; i++) {
+                int num = sc.nextInt();     // 자석의 번호
+                int d = sc.nextInt();       // 회전 방향
+                rotate(num - 1, d);
+            }
+            getAnswer();
+            System.out.println("#" + tc + " " + answer);
+        }
+    }
 }
