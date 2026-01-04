a7abc16f09c2f596b21bda7f1c304a1667bf366c
likelasttime <46569105+likelasttime@users.noreply.github.com>
2026-01-04 18:25:39 +0900

    [level 3] Title: 외벽 점검, Time: 95.33 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/README.md
+++ 프로그래머스/3/60062. 외벽 점검/README.md
@@ -1,10 +1,10 @@
 # [level 3] 외벽 점검 - 60062 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60062?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60062?language=java&gad_source=1&gad_campaignid=22366107751&gbraid=0AAAAAC_c4nDP9FClWff_voWgdMRgMfILm&gclid=CjwKCAiAmePKBhAfEiwAU3Ko3MfdMtipD4SYVgmyCmKrVJVSrgHG5ZHSGaMtndJVLN29FUE51HwpihoCUT0QAvD_BwE) 
 
 ### 성능 요약
 
-메모리: 90 MB, 시간: 59.67 ms
+메모리: 102 MB, 시간: 95.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 21일 22:56:23
+2026년 01월 04일 18:25:36
 
 ### 문제 설명
 
--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
@@ -1,76 +1,65 @@
-import java.util.Arrays;
+import java.util.*;
+import java.io.*;
+/*
+    외벽의 총 둘레는 n미터
+    레스토랑의 정북 방향 지점을 0으로 나타낸다.
+    취약 지점의 위치는 정북 방향 지점으로부터 시계 방향으로 떨어진 거리로 나타난다.
+    친구들은 출발 지점부터 시계, 반시계 방향으로 외벽을 따라서만 이동
 
+    취약 지점을 점검하기 위해 보내야 하는 친구의 수의 최소값을 구하기
+    친구들을 모두 투입해도 취약 지점을 전부 점검할 수 없는 경우에 -1
+*/
 class Solution {
-    static int[] permutation;   // dist 순열
-    static int people;      // 사람 수(dist 배열 길이)
-    static int[] distArr;   // 입력으로 받은 dist 배열
-    static int[][] weaks;   // weak 배열을 순환하는 모든 경우
-    static int weakLen;     // weak 배열 길이
-    static int answer;      // 취약 지점 점검을 위한 최소 인원 수
+    List<List<Integer>> permutationLst = new ArrayList();
     
-    public int solution(int n, int[] weak, int[] dist) {
-        people = dist.length;
-        answer = people + 1;
-        permutation = new int[people];
-        this.distArr = dist;
-        weakLen = weak.length;
-        weaks = new int[weakLen][weakLen];
-        weaks[0] = weak.clone();
-        
-        // 시작점을 바꿔가면서 모든 취약점 케이스 구하기
-        for(int start = 1; start<weakLen; start++) {    // 행
-            for(int i=1; i<weakLen; i++) {      // 열
-                weaks[start][i - 1] = weaks[start - 1][i];
-            }
-            weaks[start][weakLen - 1] = n + weaks[start - 1][0];
-        }
-        
-        dfs(new boolean[people], 0);
-        
-        if(answer == people + 1) {      // 모든 취약지점을 점검할 수 없다면
-            answer = -1;
-        }
-        return answer;
-    }
-    
-    /*
-        dist 배열의 순열 생성 후 모든 취약점에 대해 탐색
-        visit: 인덱스 = dist 배열의 인덱스
-        depth: 현재까지 고른 원소 갯수
-    */
-    public void dfs(boolean[] visit, int depth) {
-        if(depth == people) {      // 기저조건
-            for(int[] weakCase : weaks) {       // 모든 취약점 케이스 탐색
-                int weakStartIdx = 0;       // 취약 시작 지점
-                int weakEndIdx = weakStartIdx + 1;      // 취약 도착 지점
-                int person = 0;     // 사람 인덱스
-                    
-                while(weakStartIdx < weakLen && weakEndIdx < weakLen && person < people) {  // 인덱스가 초과하지 않을 동안
-                    // 한 사람이 최대한 취약 지점을 점검해야한다.
-                    while(weakEndIdx < weakLen && permutation[person] >= weakCase[weakEndIdx] - weakCase[weakStartIdx]) {
-                        weakEndIdx++;   // 다음 취약지점으로 가기
-                    }
-                    weakStartIdx = weakEndIdx;      // 취약 시작 지점 갱신 
-                    person++;       // 다음 사람으로 교체하기
-                }
-                    
-                // 모든 외벽을 점검했다면
-                if(weakStartIdx == weakLen) {
-                    answer = Math.min(answer, person);      // 투입한 사람 수 갱신
-                }
-            }
+    public void permutation(int cnt, int[] dist, boolean[] visit, ArrayList<Integer> lst) {
+        if(cnt == dist.length) {
+            permutationLst.add((List)lst.clone());
             return;
-        }
-        
-        for(int i=0; i<people; i++) {       // 모든 사람을 순서만 다르게 한 번씩 선택
-            if(visit[i]) {  // 이미 선택한 사람이라면
+        }    
+        for(int i=0; i<dist.length; i++) {
+            if(visit[i]) {
                 continue;
             }
+            lst.add(dist[i]);
             visit[i] = true;
-            permutation[depth] = distArr[i];        // depth번째에 i번 사람 선택
-            dfs(visit, depth + 1);
+            permutation(cnt + 1, dist, visit, lst);
             visit[i] = false;
-            permutation[depth] = 0;     // depth번째에 아무도 선택하지 않음
+            lst.remove(cnt);
         }
+    }
+    
+    public int solution(int n, int[] weak, int[] dist) {
+        int lenWeak = weak.length;
+        int lenDist = dist.length;
+        int answer = lenDist + 1;
+        int[] doubleWeak = new int[lenWeak * 2];
+        for(int i=0; i<lenWeak; i++) {
+            doubleWeak[i] = weak[i];
+        }
+        for(int i=0; i<lenWeak; i++) {
+            doubleWeak[i+lenWeak] = weak[i] + n;
+        }
+        
+        permutation(0, dist, new boolean[lenDist], new ArrayList());
+        for(int i=0; i<lenWeak; i++) {      // 시작점
+            for(List<Integer> p : permutationLst) {
+            	int friendCnt = 1;
+            	int idx = 0;
+            	int distance = p.get(idx) + weak[i];
+            	for(int k=i; k<i+lenWeak; k++) {
+            		if(distance < doubleWeak[k]) {
+            			friendCnt++;
+            			idx++;
+            			if(friendCnt > lenDist) {
+            				break;
+            			}
+            			distance = doubleWeak[k] + p.get(idx);
+            		}
+            	}
+            	answer = Math.min(answer, friendCnt);
+            }
+        }
+        return answer <= lenDist ? answer : -1;
     }
 }
