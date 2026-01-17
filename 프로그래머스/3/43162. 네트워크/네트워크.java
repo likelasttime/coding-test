46b647c4dda62b66e719b6f058fc3c5a2d59898c
likelasttime <46569105+likelasttime@users.noreply.github.com>
2026-01-17 14:28:01 +0900

    [level 3] Title: 네트워크, Time: 0.37 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    static int[] parent;        // 각 컴퓨터의 부모
+    
+    public static int union(int id) {
+        if(parent[id] == id) {
+            return id;
+        }
+        return parent[id] = union(parent[id]);
+    }
+    
+    public static void find(int a, int b) {
+        int aParent = union(a);
+        int bParent = union(b);
+        
+        if(aParent != bParent) {
+            parent[aParent] = bParent;
+        }
+    }
+    
+    /*
+        n: 1 <= 컴퓨터의 개수 <= 200
+        computers: 연결에 대한 정보가 담긴 2차원 배열
+        
+        네트워크 개수를 리턴
+    */
+    public int solution(int n, int[][] computers) {
+        parent = new int[n];
+        for(int i=0; i<n; i++) {
+            // 자기 자신을 부모로 초기화
+            parent[i] = i;
+        }
+        
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+            	if(computers[i][j] == 0) {		// 연결되어있지 않다면
+            		continue;
+            	}
+                find(i, j);
+            }
+        }
+        
+        for(int i=0; i<n; i++) {
+        	union(i);
+        }
+        
+        Set<Integer> set = new HashSet();
+        for(int i=0; i<n; i++) {
+        	set.add(parent[i]);
+        }
+        
+        return set.size();
+    }
+}
