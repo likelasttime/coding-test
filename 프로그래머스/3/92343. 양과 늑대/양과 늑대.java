89376f401ae4944887ee4c8429bf5cdb401333f2
likelasttime <46569105+likelasttime@users.noreply.github.com>
2026-01-25 20:21:56 +0900

    [level 3] Title: 양과 늑대, Time: 3.27 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,43 +1,54 @@
-class Solution {
-    static int answer = 1;
-    static int[] infoArr;
-    static int[][] edgesArr;
-    
-    public void dfs(int sheep, int wolf, int cur, boolean[] visit) {
-        visit[cur] = true;
-        
-        if(infoArr[cur] == 0) {     // 양이 있으면
-            sheep++;
-            answer = Math.max(answer, sheep);
-        } else {
-            wolf++;
-        }
-        
-        if(sheep <= wolf) {     // 늑대가 더 많다면
-            return;
-        } 
-            
-        for(int[] edge : edgesArr) {
-            int parent = edge[0];
-            int child = edge[1];        // 자식 노드 번호
+import java.util.*;
 
-            if(visit[parent] && !visit[child]) {       // 부모는 방문했고, 방문하지 않은 자식이라면
-                boolean[] newVisit = new boolean[visit.length];
-                for(int j=0; j<visit.length; j++) {
-                    newVisit[j] = visit[j];
-                }
-                dfs(sheep, wolf, child, newVisit);
-            }
+/* 이진 트리 모양의 각 노드에 늑대와 양이 한 마리씩 놓여 있다. 
+루트 노드에서 출발하여 각 노드를 돌아다니며 양을 모으려 한다. 
+각 노드를 방문할 때 마다 해당 노드에 있던 양과 늑대가 따라온다. 
+모은 양의 수보다 늑대의 수가 같거나 더 많아지면 모든 양을 잡아먹는다. 
+최대한 많은 수의 양을 모아서 다시 루트 노드로 돌아가기 
+*/
+class Solution {
+    final static int WOLF = 1;
+    final static int SHEEP = 0;
+
+    static List<Integer>[] tree; // 자식 리스트
+    static int[] arr;            // info 배열
+    static int answer;
+
+    public void dfs(int wolf, int sheep, List<Integer> candidates) {
+        answer = Math.max(answer, sheep);
+
+        for (int i = 0; i < candidates.size(); i++) {
+            int cur = candidates.get(i);
+
+            int nextWolf = wolf + (arr[cur] == WOLF ? 1 : 0);
+            int nextSheep = sheep + (arr[cur] == SHEEP ? 1 : 0);
+
+            if (nextWolf >= nextSheep) continue;
+
+            List<Integer> next = new ArrayList<>(candidates);
+            next.remove(i);
+            next.addAll(tree[cur]);
+
+            dfs(nextWolf, nextSheep, next);
         }
     }
 
     public int solution(int[] info, int[][] edges) {
-        int n = info.length;    // 노드의 갯수
-        infoArr = info;
-        edgesArr = edges;
+        int n = info.length;
+        arr = info;
+        answer = 0;
 
-        dfs(0, 0, 0, new boolean[n]);
+        tree = new ArrayList[n];
+        for (int i = 0; i < n; i++) tree[i] = new ArrayList<>();
 
+        for (int[] edge : edges) {
+            tree[edge[0]].add(edge[1]);
+        }
+
+        List<Integer> start = new ArrayList<>();
+        start.add(0); // 루트부터 시작
+
+        dfs(0, 0, start);
         return answer;
     }
 }
