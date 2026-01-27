69116ea10881e496a7d828dc647174328b3683e9
likelasttime <46569105+likelasttime@users.noreply.github.com>
2026-01-27 20:36:56 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 52.80 ms, Memory: 208 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/README.md
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/README.md
@@ -1,10 +1,10 @@
 # [level 3] 파괴되지 않은 건물 - 92344 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92344?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92344) 
 
 ### 성능 요약
 
-메모리: 213 MB, 시간: 56.55 ms
+메모리: 208 MB, 시간: 52.80 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 10일 23:51:57
+2026년 01월 27일 20:36:53
 
 ### 문제 설명
 
--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,63 +1,66 @@
+/*
+    N * M 행렬 모양의 게임 맵
+    내구도를 가진 건물이 각 칸마다 하나씩 있다.
+    건물은 적의 공격을 받으면 내구도가 감소
+    내구도 <= 0:
+        파괴
+    회복 스킬을 사용하면 건물들의 내구도를 높인다.
+    적의 공격과 아군의 회복 스킬은 항상 직사각형 모양이다.
+    내구도가 0이하가 된 이미 파괴된 건물도 공격을 받으면 계속해서 내구도가 하락한다.
+    적의 공격 혹은 아군의 회복 스킬이 모두 끝난 뒤 파괴되지 않는 건물의 개수를 구하기
+*/
 class Solution {
+    static final int ATTACK = 1;
+    static final int RECOVERY = 2;
+
     public int solution(int[][] board, int[][] skill) {
         int N = board.length;
         int M = board[0].length;
 
-        // 누적합을 계산한 배열을 얻음
-        int[][] sum = makeSumArr(skill, N, M);
+        // 차분 배열
+        int[][] diff = new int[N + 1][M + 1];
 
+        // 스킬 적용 (O(K))
+        for (int[] s : skill) {
+            int type = s[0];
+            int r1 = s[1];
+            int c1 = s[2];
+            int r2 = s[3];
+            int c2 = s[4];
+            int degree = s[5];
+
+            if (type == ATTACK) degree = -degree;
+
+            diff[r1][c1] += degree;
+            diff[r1][c2 + 1] -= degree;
+            diff[r2 + 1][c1] -= degree;
+            diff[r2 + 1][c2 + 1] += degree;
+        }
+
+        // 가로 누적합
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j < M; j++) {
+                diff[i][j] += diff[i][j - 1];
+            }
+        }
+
+        // 세로 누적합
+        for (int j = 0; j < M; j++) {
+            for (int i = 1; i < N; i++) {
+                diff[i][j] += diff[i - 1][j];
+            }
+        }
+
+        // board 반영 + 정답 계산
         int answer = 0;
-        // board와 sum 배열을 더해서 살아남은 칸의 개수를 셈
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                // board의 값과 skill의 영향을 합친 결과가 0보다 큰지 확인
-                if (board[i][j] + sum[i][j] > 0) {
+                if (board[i][j] + diff[i][j] > 0) {
                     answer++;
                 }
             }
         }
 
         return answer;
-    }
-
-    private static int[][] makeSumArr(int[][] skill, int N, int M) {
-        int[][] result = new int[N + 1][M + 1];
-
-        // skill 배열을 통해 결과 배열에 영향을 반영
-        for (int i = 0; i < skill.length; i++) {
-            int type = skill[i][0];
-            int degree = skill[i][5];
-            if (type == 1) {  // 공격
-                degree *= -1;
-            }
-
-            // (startX, startY)부터 (endX, endY)까지 영향을 반영
-            result[skill[i][1]][skill[i][2]] += degree;
-            if (skill[i][4] + 1 < M) {
-                result[skill[i][1]][skill[i][4] + 1] -= degree;
-            }
-            if (skill[i][3] + 1 < N) {
-                result[skill[i][3] + 1][skill[i][2]] -= degree;
-            }
-            if (skill[i][3] + 1 < N && skill[i][4] + 1 < M) {
-                result[skill[i][3] + 1][skill[i][4] + 1] += degree;
-            }
-        }
-
-        // 가로 누적합 계산
-        for (int i = 0; i < N + 1; i++) {
-            for (int j = 1; j < M + 1; j++) {
-                result[i][j] += result[i][j - 1];
-            }
-        }
-
-        // 세로 누적합 계산
-        for (int j = 0; j < M + 1; j++) {
-            for (int i = 1; i < N + 1; i++) {
-                result[i][j] += result[i - 1][j];
-            }
-        }
-
-        return result;
     }
 }
